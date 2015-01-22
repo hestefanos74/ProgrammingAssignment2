7b4246f4777e32e84eff75514ad485f17d1ce95b
Hany Estefanos <hany.estefnos@gmail.com>
2015-01-22 23:57:39 +0200

    Adding cacheSolve descriptions
    Complete description for both functions,
    complete function : cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -5,21 +5,41 @@
 
 makeCacheMatrix <- function(x = matrix()){ ## Creating the 
   ## function with argument 'x' as an empty matrix.
-  matinv <- matrix() ## Assign initial vector for the matrix
+  matinv <- NULL ## Assign initial vector for the matrix
   ## matinv as empty matrix with allocated memory space.
   set <- function(y = matrix()) {## a function to make the calling 
     ##  of the vector varible applied from the parent environment
     ## i.e the glopal environment (cached memory).
     ## Also it used to change the cached matrix.
     x <<- y  ## Assigning the argument x to the calling environment
-    matinv <<- matrix() ## Re-initialize the initial vector of
+    ## and assign the free marix vector to the entered matrix
+    matinv <<- NULL ## Re-initialize the initial vector of
     ## the cached matrix.
   }
-  get <- function() x 
+  get <- function() x  ## Get the victor of the  matrix
+  setmatrix<-function(solve)  matinv <<- solve ## Set the cached 
+  ## to be the inverse of the matrix and assign it to memory
+  getmatrix<-function() matinv ## Get the cached matrix inverse
+  list(set=set, get=get,   ## List the current environment of the 
+       setmatrix=setmatrix,  ## functions.
+       getmatrix=getmatrix)
 }
 
 
 
-cacheSolve <- function(x,...){
-  ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(x, ...){   ## Return a matrix that
+  ## is the inverse of 'x'
+  matinv <- x$getmatrix() ## Get the inverse matrix in the cache
+  if(!is.null(matinv)){ ## If the cache inverse matrix exist
+    message("getting cached data") ## Print that message
+    return(matinv) ## Print the cached inverse matrix.
+  }
+  matrix <- x$get() ## If the cached inverse matrix is empty
+  ## Get the matrix entered.
+  matinv <- solve(matrix, ...) ## Calculate the matrix inverse
+  ## and assign it to the cache inverse matrix
+  x$setmatrix(matinv) ## Set the current matrix as the cached one
+  matinv ## Print the calculated  inverse matrix (cached from
+  ## previous line)
 }
