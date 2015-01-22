f051d70eb411fce9e399d1742376e22a4ee91b98
Hany Estefanos <hany.estefnos@gmail.com>
2015-01-22 04:29:27 +0200

    Revert "Initial commit for Programming Assignment 2"
    This reverts commit 27987823fcf81d46a5e2186391addd87c079b879.
    
    Conflicts:
    	README.md
    	cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## "makeCacheMatrix" is a function that creates a special
+## matrix object which can cache its matrix inverse.
+## The condition for the function  to run is that the 
+## matrix entered be a square invertible matrix.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()){ ## Creating the 
+  ## function with argument 'x' as an empty matrix.
+  matinv <- matrix() ## Assign initial vector for the matrix
+  ## matinv as empty matrix with allocated memory space.
+  set <- function(y = matrix()) {## a function to make the calling 
+    ##  of the vector varible applied from the parent environment
+    ## i.e the glopal environment (cached memory).
+    ## Also it used to change the cached matrix.
+    x <<- y  ## Assigning the argument x to the calling environment
+    matinv <<- matrix() ## Re-initialize the initial vector of
+    ## the cached matrix.
+  }
+  get <- function() x 
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...){
+  ## Return a matrix that is the inverse of 'x'
 }
